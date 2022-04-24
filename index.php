4d4550346dbf93a8ee82e8ac6549575c54f2c73d
Viveklincis <102012900+Viveklincis@users.noreply.github.com>
2022-04-24 07:13:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 session_start();
-$connect = mysqli_connect("vivekweb.mysql.database.azure.com", "vivek", "Gayan@29120402", "vivek");
+$mysqli = new mysqli("vivekweb.mysql.database.azure.com", "vivek", "Gayan@29120402", "vivek");
 
 // Check connection
 if ($mysqli -> connect_errno) {
